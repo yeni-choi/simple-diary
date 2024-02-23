2ec4a801002b2ad84b5a7c6520213abb39d9342f
yeni-choi <yewonchoi@ewhain.net>
2024-02-23 17:39:59 +0900

    refactor: remove dummy datalist

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
+import { useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-
-const dummyList = [
-  {
-    id: 1,
-    author: "yewon",
-    content: "content1",
-    emotion: 5,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 2,
-    author: "choi",
-    content: "content2",
-    emotion: 5,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 3,
-    author: "yeni",
-    content: "content3",
-    emotion: 5,
-    created_date: new Date().getTime()
-  }
-];
 
 function App() {
   return (
