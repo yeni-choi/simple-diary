6094f23395e7417c577442fc2c3313f4e741c5f3
yeni-choi <yewonchoi@ewhain.net>
2024-02-23 17:13:33 +0900

    feat: Add list rendering in DiaryList

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,10 +1,21 @@
+import DiaryItem from './DiaryItem.js'
+
 const DiaryList = ({ diaryList }) => {
     return (
       <div className="DiaryList">
         <h2>Diary List</h2>
         <h4>total: {diaryList.length}</h4>
+        <div>
+          {diaryList.map((it) => (
+            <DiaryItem key={`diaryitem_${it.id}`} {...it} />
+          ))}
+        </div>
       </div>
     );
   };
   
+  DiaryList.defaultProps={
+    diaryList:[],
+  };
+
   export default DiaryList;
