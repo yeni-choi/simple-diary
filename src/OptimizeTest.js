99cbd4259202d916e879d9afa23d09364beb1d7d
yeni-choi <yewonchoi@ewhain.net>
2024-02-27 23:43:45 +0900

    chore: create MemoizedComponent with reactMemo

---
+++ src/OptimizeTest.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+const CounterA = React.memo(({ count }) => {
+  useEffect(() => {
+    console.log(`CountA Update - count : ${count}`);
+  });
+  return <div>{count}</div>;
+});
+
+const CounterB = ({ obj }) => {
+  useEffect(() => {
+    console.log(`CountB Update - count : ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps, nextProps) => {
+  if (prevProps.obj.count === nextProps.obj.count) {
+    return true;
+  }
+  return false;
+};
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
+const OptimizeTest = () => {
+  const [count, setCount] = useState(1);
+  const [obj, setObj] = useState({
+    count: 1
+  });
+
+  return (
+    <div style={{ padding: 50 }}>
+      <div>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A Button</button>
+      </div>
+      <div>
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj} />
+        <button onClick={() => setObj({ count: 1 })}>B Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default OptimizeTest;
